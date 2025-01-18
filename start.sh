68a53fb494b6964a7be0c3e607cecf021ed78914
Ubuntu <azureuser@Eden-Shack.4gqumai2l2terlbnyhrs0hfpch.cx.internal.cloudapp.net>
2025-01-18 21:03:26 +0000

    modified start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+SESSIONNAME="uofthacks12-backend"
+# Check if the session exists
+tmux has-session -t $SESSIONNAME 2>/dev/null
+source .venv/bin/activate
+pip install -r requirements.txt
+uvicorn index:app --host 0.0.0.0 --port 8000 --reload
+#uvicorn index:app --reload --port 8000                                                                                                                  
